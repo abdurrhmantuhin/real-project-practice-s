e2a5e2dcf07516a8684fb5c2c0682468b16f112b
Abdur Rhman Tuhin <sskabdurrhman@gmail.com>
2025-09-24 07:06:24 +0600

    CHAPTER 2: STRING SLICING - THE FOUNDATION OF TEXT PROCESSING

---
+++ apply and try.py
@@ -0,0 +1,18 @@
+# x = "hello world my name is Abdur Rhman Tuhin"
+
+# print(F"1. charecters: {x[-5:]}")
+# print(F"2. charecters: {x[5:]}")
+# print(F"3. charecters: {x[:5]}")
+# print(F"4. charecters: {x[::-2]}")
+# print(F"5. charecters: {x[::2]}")
+# print(F"6. charecters: {x[5:15]}")
+# print(f"7. x len: {len(x)} charecters")
+
+
+def hello():
+    x = "hello"
+    y= print(x)
+    return y
+
+g = hello()
+print(g)
